68a0e52b1a05dd1ca593f501a18f2c791c5f2b20
progmamun <mamun333089@gmail.com>
2026-06-22 21:57:11 +0600

    updated

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,13 +6,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    '/dashboard',
-    '/dashboard/:path*',
-    '/login',
-    // 1. Skip all Next.js internals (_next) and static asset files
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // 2. Always ensure it processes API and tRPC endpoints
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/dashboard', '/dashboard/:path*', '/login', '/api/auth/refresh'],
 }
